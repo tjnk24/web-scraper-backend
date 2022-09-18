488f11a220e8fe8350ab7c08461c43efb811f959
Alexander M <tjnk24@gmail.com>
2022-09-18 18:29:20 +0400

    vscode settings, versioning

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import {VersioningType} from '@nestjs/common';
 import {NestFactory} from '@nestjs/core';
 
 import {AppModule} from './app.module';
 
 const bootstrap = async () => {
     const app = await NestFactory.create(AppModule);
+
+    app.enableVersioning({
+        type: VersioningType.URI,
+        defaultVersion: '1',
+    });
 
     await app.listen(3000);
 };
